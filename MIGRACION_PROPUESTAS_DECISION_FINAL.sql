42a4f3eb3525dcf7af36e764ee2653333b3dea19
MeraKremaMusic <merakremamusic@gmail.com>
2026-08-27 13:06:23 -0500

    Limpiar archivos antiguos del repositorio

--- MIGRACION_PROPUESTAS_DECISION_FINAL.sql
+++
@@ -1,4 +0,0 @@
--- Permite conservar el historial de propuestas aunque el audio haya sido eliminado.
-ALTER TABLE `propuestas`
-  MODIFY `audio_url` VARCHAR(1000) NULL,
-  MODIFY `audio_public_id` VARCHAR(255) NULL;
